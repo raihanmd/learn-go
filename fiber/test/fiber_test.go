158c445e0aa16ae6a69272e92f8bc38dade38c0e
raihanmd <mraihanf471@gmail.com>
2024-09-09 21:19:40 +0700

    ongoing: learning fiber

--- fiber/test/fiber_test.go
+++ fiber/test/fiber_test.go
@@ -1,7 +1,10 @@
 package test
 
 import (
+	"encoding/json"
 	"go-fiber/app"
+	"go-fiber/model/web"
+	"go-fiber/model/web/response"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -19,6 +22,24 @@ func TestFiberApp(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, 200, res.StatusCode)
 
+	var response web.WebSuccessResponse[response.User]
+
 	bytes, _ := io.ReadAll(res.Body)
-	assert.Equal(t, "Hello, World!", string(bytes))
+	json.Unmarshal(bytes, &response)
+
+	assert.Equal(t, response.Code, 200)
+	assert.Equal(t, response.Message, "OK")
+	assert.Equal(t, response.Payload.ID, 1)
+	assert.Equal(t, response.Payload.Name, "Raihanmd")
+	assert.Equal(t, response.Payload.Email, "Raihanmd")
+}
+
+func TestHeader(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/protected", nil)
+	req.Header.Add("Authorization", "admin")
+
+	res, err := router.Test(req)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, res.StatusCode)
 }
